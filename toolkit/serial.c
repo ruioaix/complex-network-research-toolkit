e327d65282e5b1746cdc1f5f3e183edb036ad2b8
xrfind <xrfind@gmail.com>
2014-07-01 22:00:48 +0200

    find out what happens to serial

--- toolkit/serial.c
+++ toolkit/serial.c
@@ -7,20 +7,21 @@ int main(int argc, char **argv)
 {
 	if (argc != 3) isError("argc");
 	struct LineFile *lf = create_LineFile(argv[1], 1, 1, -1);
-	struct HT *ht1 = create_HT(1000000);
-	struct HT *ht2 = create_HT(1000000);
+	struct HT *ht1 = create_HT(100000000);
+	//struct HT *ht2 = create_HT(100000000);
 	long i;
 	for (i = 0; i < lf->linesNum; ++i) {
 		insert_HE_HT(ht1, lf->i1[i]);	
-		insert_HE_HT(ht2, lf->i2[i]);	
+		insert_HE_HT(ht1, lf->i2[i]);	
+		//insert_HE_HT(ht2, lf->i2[i]);	
 	}
 	FILE *fp = sfopen(argv[2], "w");
 	for (i = 0; i < lf->linesNum; ++i) {
-		fprintf(fp, "%d\t%d\n", get_index_HT(ht1, lf->i1[i]), get_index_HT(ht2, lf->i2[i]));
+		fprintf(fp, "%d\t%d\n", get_index_HT(ht1, lf->i1[i]), get_index_HT(ht1, lf->i2[i]));
 	}
 	fclose(fp);
 	free_HT(ht1);
-	free_HT(ht2);
+	//free_HT(ht2);
 	free_LineFile(lf);
 
 	return 0;
