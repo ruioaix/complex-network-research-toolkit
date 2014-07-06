c281002bb6f5659ad841c72c6e60169cca3db342
xrfind <xrfind@gmail.com>
2014-07-06 15:27:04 +0200

    minor

--- toolkit/serial.c
+++ toolkit/serial.c
@@ -10,21 +10,28 @@ int main(int argc, char **argv)
 	struct LineFile *lf = create_LineFile(argv[1], 1, 1, -1);
 	struct Net *net = create_Net(lf);
 	struct HT *ht1 = create_HT(100000000);
-	//struct HT *ht2 = create_HT(100000000);
 	long i;
 	for (i = 0; i < lf->linesNum; ++i) {
 		insert_HE_HT(ht1, lf->i1[i]);	
 		insert_HE_HT(ht1, lf->i2[i]);	
-		//insert_HE_HT(ht2, lf->i2[i]);	
 	}
-	FILE *fp = sfopen(argv[2], "w");
-	for (i = 0; i < net->maxId + 1; ++i) {
-		fprintf(fp, "%d\t%d\n", i, get_index_HT(ht1, i));
+
+	FILE *fp = sfopen("/tmp/originid_nowid", "w");
+	int j;
+	for (j = 0; j < net->maxId + 1; ++j) {
+		fprintf(fp, "%d\t%d\n", j, get_index_HT(ht1, j));
 	}
 	fclose(fp);
+
+	fp = sfopen(argv[2], "w");
+	for (i = 0; i < lf->linesNum; ++i) {
+		fprintf(fp, "%d\t%d\n", get_index_HT(ht1, lf->i1[i]), get_index_HT(ht1, lf->i2[i]));
+	}
+	fclose(fp);
+
 	free_HT(ht1);
-	//free_HT(ht2);
 	free_LineFile(lf);
+	free_Net(net);
 
 	return 0;
 }
