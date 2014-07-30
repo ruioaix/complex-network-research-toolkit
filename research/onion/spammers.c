bf25e701681e40512719ce0944ee2c20cc2aaa6d
xrfind <xrfind@gmail.com>
2014-07-30 16:46:43 +0200

    minor

--- research/onion/spammers.c
+++ research/onion/spammers.c
@@ -4,6 +4,8 @@
 int main(int argc, char **argv) {
 	struct LineFile *lf = create_LineFile("/tmp/leadership_data", 1, 1, -1);
 	struct Net *net = create_directed_Net(lf);
+	free_LineFile(lf);
+	free_Net(net);
 
 	return 0;
 }
