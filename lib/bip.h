2fb74363fbf9cb6c7c4de2a179a5b856efde86a2
xrfind <xrfind@gmail.com>
2014-08-01 17:14:05 +0200

    bip

--- lib/bip.h
+++ lib/bip.h
@@ -8,13 +8,7 @@
  * 		(BTW, the origin LineFile is getting from a file which contain at least two int integer in an line, 
  * 		to a line containing three or more integers, only fetch two.  using create_LineFile(char *filename, 1, 1, -1))
  *
- * 		struct Bip contains only half information of the bipartite. 
- * 		(I mean one struct Bip can store a LineFile completely, but it doesn't store detail information.)
- * 		you need two struct Bip to store all detail information of a Bipartite.
- * 		one is indexed by i1(user).
- * 		one is indexed by i2(item).
- *
- * struct Metrics_Bip contains the result(all metrics) for all kinds of recommendation algorithm.
+ * struct Metrics_Bip contains the results(all metrics) for all kinds of recommendation algorithm.
  *
  */
 #ifndef CN_BIP_H
@@ -25,35 +19,29 @@
 
 //this struct is just one way for describing Bipartite.
 //other ways like (int ***xx) is also usefully.
-//Bipartite contains two parts. e.g. user and item.
-//but here, create_Bipartite only create user Bipartite or item Bipartite.
-//if you want both, create two time with different index arg.
 struct HalfBip {
 	int maxId, minId, idNum;
 	int degreeMax, degreeMin;
 	int *degree, **edges;
 };
-
 struct Bip {
 	long edgesNum;
 	struct HalfBip *left;
 	struct HalfBip *right;
 };
 
-//if index is 1, means the i1 is the index, and i2 is the data saved into (int **edges).
-//i1 and i2 is the data in LineFile.
-//if index is 2, means i2 is the index.
 void free_Bip(struct Bip *bip);
 struct Bip *create_Bip(const struct LineFile * const file);
 void sort_desc_half_Bip(struct HalfBip *hbip);
+
 struct Bip * clone_Bip(struct Bip *bip);
 //void verify_Bip(struct Bip *bipi1, struct Bip *bipi2);
 void print_Bip(struct Bip *bip, char *filename);
 
 //the dividation will guarantee that: 
-//	for each available user(degree of this user is at least one), at least there will be an edge in big part.
-//	for each available item(degree of this item is at least one), at least there will be an edge in big part.
-//so maybe some users are not existed in the small part.
+//	for each available user(degree of this user is at least one), at least there will be one edge in big part.
+//	for each available item(degree of this item is at least one), at least there will be one edge in big part.
+//so maybe some users/items are not existed in the small part.
 void divide_Bip(struct Bip *bip, double rate, struct LineFile **small_part, struct LineFile **big_part);
 
 //if target == 1, then calculate i1(mostly could be user)'s similarity.
